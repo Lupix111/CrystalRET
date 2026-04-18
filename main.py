2b147aba8aefebf5c128a50d80a498c8b8d09934
Lupix111 <em4n.carlino@gmail.com>
2026-04-19 00:02:42 +0200

    Testrun, nothing serious, just installed the requirements and libs after fighting VS for some fucking libs.

---
+++ main.py
@@ -0,0 +1,15 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("CrystalRET")
+
+app = QApplication()
+window=MainWindow()
+window.show()
+app.exec()
+
+     
