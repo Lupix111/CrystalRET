c673673e5f6f77d3c3c3fb94dabcce71dacb0dd9
Lupix111 <em4n.carlino@gmail.com>
2026-04-19 21:03:56 +0200

    version 0.1, pre-alpha. Nothing is there but the base work has been laid so i guess it's worth the merge

--- adapterSTT_audioTrigger.py
+++ adapterSTT_audioTrigger.py
@@ -3,7 +3,7 @@ import STT
 import audioTrigger
 
 def esegui_analisi(self, file_mp3):
-    """Questa funzione gira 'dietro le quinte' (in background)"""
+    #Questa funzione gira in background
     print(f"Analisi AI iniziata per {file_mp3}...")
     
     # 1. Trascrizione con la classe STT
@@ -11,13 +11,13 @@ def esegui_analisi(self, file_mp3):
     
     if testo:
         print(f"RISULTATO AI: {testo}")
-        # 2. Qui chiamerai Ollama per il nominativo (prossimo step)
+        # 2. Qui chiamerò Ollama per il nominativo (prossimo step)
         # 3. Log su file di testo
         with open("log_radio.txt", "a") as f:
             f.write(f"[{file_mp3}] {testo}\n")
 
 def gestisci_fine_trasmissione(self, file_mp3):
-    """Viene chiamato appena il file MP3 è pronto"""
+    #Viene chiamato appena il file MP3 è pronto
     
     # Creiamo un Thread che si occupa della trascrizione
     # così il loop principale della radio non si ferma!
